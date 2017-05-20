f7fd16891a944058d3a74fd0423539c319bde8ea
Francis <francis.isabel@outlook.com>
2017-05-19 20:52:00 -0400

    Update db.php

--- db.php
+++ db.php
@@ -19,10 +19,10 @@
 <?php
 
 if(!$_ENV["VCAP_SERVICES"]){ //local dev
-    $mysql_server_name = "127.0.0.1:3306";
+    $mysql_server_name = "172.17.0.21:3306";
     $mysql_username = "root";
     $mysql_password = "";
-    $mysql_database = "test";
+    $mysql_database = "sampledb";
 } else { //running in Bluemix
     $vcap_services = json_decode($_ENV["VCAP_SERVICES" ]);
     if($vcap_services->{'mysql-5.5'}){ //if "mysql" db service is bound to this application
